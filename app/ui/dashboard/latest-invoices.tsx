53743441aae0c0704a9b6a2db1b2e9a312933eb1
aygull <aygulka13@gmail.com>
2024-06-09 11:53:53 +0200

    changed logo

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -6,7 +6,7 @@ import { HeartIcon } from '@heroicons/react/24/outline';
 export default async function XRP() {
   return (
     <div className="flex w-full flex-col md:col-span-4">
-      <div className='flex centered align-baseline'>
+      <div className='flex centered align items-baseline'>
         <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
           XRP savings
         </h2>
